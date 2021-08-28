abd5172c639cf354c599ec45c108616ed5e94398
nmessner <niklas.messner001@stud.fh-dortmund.de>
2021-08-28 13:27:36 +0200

    New Parts 2

--- lib/layouts.dart
+++ lib/layouts.dart
@@ -4,9 +4,25 @@ Widget mainWidget(){
   return Row(
 
       children: [
-        Image.network('https://picsum.photos/100'),
-        Image.network('https://picsum.photos/100'),
-        Image.network('https://picsum.photos/100')
+        container(),
+        container()
       ]
+  );
+}
+
+Widget container(){
+  return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0x85A02525),
+          border: Border.all(
+            color: const Color(0x85000000),
+            width: 5,
+          ),
+          borderRadius: BorderRadius.circular(4)
+        ),
+        child: Image.network('https://picsum.photos/100'),
+        margin: const EdgeInsets.all(4),
+      )
   );
 }
