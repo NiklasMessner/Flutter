6d5476fbe1cd2cd30279b4934f365fff384628eb
nmessner <niklas.messner001@stud.fh-dortmund.de>
2021-08-28 14:02:07 +0200

    New Parts 3

--- lib/layouts.dart
+++ lib/layouts.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/cupertino.dart';
 
 Widget mainWidget(){
-  return Row(
-
-      children: [
-        container(),
-        container()
-      ]
-  );
+  return gridView();
 }
+
+Widget gridView() {
+  return GridView.extent(
+    maxCrossAxisExtent: 150, 
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridList(25),
+  );    
+}
+
+List<Container>_buildGridList(int i) => List.generate(
+    i, (index) => Container(
+      child: Image.network('https://picsum.photos/100/'),
+)
+);
+
 
 Widget container(){
   return Expanded(
